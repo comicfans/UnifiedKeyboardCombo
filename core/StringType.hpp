2cc50966a47e6b2f0bfbf06edfe38ccc40b3de9c
comicfans <comicfans44@gmail.com>
2014-12-11 22:38:43 +0000

    temp work

---
+++ core/StringType.hpp
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  StringType.hpp
+ *
+ *    Description:  
+ *
+ *
+ *        Version:  1.0
+ *        Created:  2014年12月11日 22时13分37秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef STRINGTYPE_HPP_ZB9ONXIJ
+#define STRINGTYPE_HPP_ZB9ONXIJ
+
+#include <string>
+
+#if __WIN32
+#include <tchar.h>
+    using StringType=std::wstring;
+#else
+    using StringType=std::string;
+#define _T 
+#endif
+
+
+#endif /* end of include guard: STRINGTYPE_HPP_ZB9ONXIJ */
